4a50b1ef2eb5b2fbda459dddf540c0b294b58b4e
qritkl <qritkl@gmail.com>
2026-03-17 19:29:37 -0300

    ajustes decentes

--- rnCB.py
+++ rnCB.py
@@ -6,31 +6,32 @@ from time import sleep
 # sera usado para gerar strings aleatorias
 CHARS = ascii_letters+digits
 
-# criacao e copia dos arquivos do diretorio atual
-system("mkdir renomeados")
-originais = listdir()
-originais.remove("rnCB.py")
-originais.remove("renomeados")
+if __name__=="__main__":
+	# criacao e copia dos arquivos do diretorio atual
+	system("mkdir renomeados")
+	originais = listdir()
+	originais.remove("rnCB.py")
+	originais.remove("renomeados")
 
-# detecta arquivos de formatacao invalida
-invalidos = []
-for x in originais:
-	if len(x)<6 or (len(x)>6 and x[6] != "."):
-		print(f"arquivo {x} nao corresponde a formatacao catbox")
-		invalidos.append(x)
+	# detecta arquivos de formatacao invalida
+	invalidos = []
+	for x in originais:
+		if len(x)<6 or (len(x)>6 and x[6] != "."):
+			print(f"arquivo {x} nao corresponde a formatacao catbox")
+			invalidos.append(x)
 
-# remove invalidos da lista de arquvos que serao alterados
-for x in invalidos:
-	originais.remove(x)
+	# remove invalidos da lista de arquvos que serao alterados
+	for x in invalidos:
+		originais.remove(x)
 
-for x in originais:
-	system(f"cp {x} renomeados")
+	for x in originais:
+		system(f"cp {x} renomeados")
 
-# renomeia arquivos copiados com string aleatoria de 20 caracteres
-for x in originais:
-	nome=""
-	for y in range(20):
-		nome += choice(CHARS)
-	extensao = x[6:]
-	novo_nome = nome + extensao
-	system(f"mv renomeados/{x} renomeados/{novo_nome}")
+	# renomeia arquivos copiados com string aleatoria de 20 caracteres
+	for x in originais:
+		nome=""
+		for y in range(20):
+			nome += choice(CHARS)
+		extensao = x[6:]
+		novo_nome = nome + extensao
+		system(f"mv renomeados/{x} renomeados/{novo_nome}")
